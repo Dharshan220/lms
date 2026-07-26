9b8e52d493674480a23c86b547d12197fd27d97b
Dharshan <dharshane21@gmail.com>
2026-07-26 15:22:43 +0530

    Set social media links to actual Nano Spark URLs

--- config/nanospark.php
+++ config/nanospark.php
@@ -10,21 +10,21 @@ return [
     | Update these values in your .env file to configure social media links
     | and contact information used across the public site.
     |
-    | NANOSPARK_EMAIL=hello@nanospark.org
-    | NANOSPARK_LINKEDIN=https://linkedin.com/company/nanospark
-    | NANOSPARK_YOUTUBE=https://youtube.com/@nanospark
-    | NANOSPARK_FACEBOOK=https://facebook.com/nanospark
-    | NANOSPARK_INSTAGRAM=https://instagram.com/nanospark
+    | NANOSPARK_EMAIL=nanospark46@gmail.com
+    | NANOSPARK_LINKEDIN=https://www.linkedin.com/in/nano-spark-4300a23bb
+    | NANOSPARK_YOUTUBE=https://youtube.com/@nanosparkbytes
+    | NANOSPARK_FACEBOOK=https://www.facebook.com/profile.php?id=61592291288490
+    | NANOSPARK_INSTAGRAM=https://www.instagram.com/nano_spark_
     |
     */
 
-    'email' => env('NANOSPARK_EMAIL', 'hello@nanospark.org'),
+    'email' => env('NANOSPARK_EMAIL', 'nanospark46@gmail.com'),
 
     'social' => [
-        'linkedin' => env('NANOSPARK_LINKEDIN', 'https://linkedin.com/company/nanospark'),
-        'youtube' => env('NANOSPARK_YOUTUBE', 'https://youtube.com/@nanospark'),
-        'facebook' => env('NANOSPARK_FACEBOOK', 'https://facebook.com/nanospark'),
-        'instagram' => env('NANOSPARK_INSTAGRAM', 'https://instagram.com/nanospark'),
+        'linkedin' => env('NANOSPARK_LINKEDIN', 'https://www.linkedin.com/in/nano-spark-4300a23bb'),
+        'youtube' => env('NANOSPARK_YOUTUBE', 'https://youtube.com/@nanosparkbytes'),
+        'facebook' => env('NANOSPARK_FACEBOOK', 'https://www.facebook.com/profile.php?id=61592291288490'),
+        'instagram' => env('NANOSPARK_INSTAGRAM', 'https://www.instagram.com/nano_spark_'),
     ],
 
 ];
